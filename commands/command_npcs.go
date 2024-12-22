dda76a609d8700807737098cfc52df9511ca64ce
Jake Abed <jakeabed@proton.me>
2024-12-21 19:28:21 -0800

    Add charm huh and add npc flag

--- commands/command_npcs.go
+++ commands/command_npcs.go
@@ -4,12 +4,25 @@ import (
 	"context"
 	"fmt"
 	"github.com/jake-abed/auxquest/internal/db"
+	"os"
+	"github.com/charmbracelet/huh"
+	"strconv"
 )
 
 func commandNpcs(s *State) error {
 	npcArgs := s.Args[1:]
 	if len(npcArgs) == 0 {
 		fmt.Println("Npcs command expects at least one argument!")
+		os.Exit(0)
+	}
+
+	if len(npcArgs) == 1 && npcArgs[0] == "-a" {
+		err := addNpc(s)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		os.Exit(0)
 	}
 
 	testNpc := &db.NpcParams{
@@ -34,4 +47,136 @@ func commandNpcs(s *State) error {
 	fmt.Println(npc)
 
 	return nil
+}
+
+func npcHelp() {
+	intro := "AuxQuest Npc Help\n"
+	introTip := "Monsters subcommands information"
+	fmt.Println(header.Render(intro + introTip))
+	add := bold.Render("  *** npc -a <npc-name> | ")
+	addMessage := "Add a new npc by name."
+	fmt.Println(add + addMessage)
+	fight := bold.Render("  *** monsters -f <monster-1> <monster-2> | ")
+	fightMessage := "Simulate a fight between monsters.\n"
+	fmt.Println(fight + fightMessage)
+
+}
+
+func addNpc(s *State) error {
+	var name string
+	var race string
+	var class string
+	var subclass string
+	var alignment string
+	var sex string
+	var desc string
+	var languages string
+	var level string
+	var hitpoints string
+
+	npcForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Name").
+				Value(&name).
+				Validate(func(s string) error {
+					if s == "" {
+						return fmt.Errorf("You have to enter a name!")
+					}
+					return nil
+				}),
+
+			huh.NewInput().
+				Title("Race").
+				Value(&race),
+
+			huh.NewInput().
+				Title("Class").
+				Value(&class),
+
+			huh.NewInput().
+				Title("Subclass").
+				Value(&subclass),
+
+			huh.NewInput().
+				Title("Alignment").
+				Value(&alignment),
+
+			huh.NewSelect[string]().
+				Title("Sex").
+				Options(
+					huh.NewOption("Male", "male"),
+					huh.NewOption("Female", "female"),
+					huh.NewOption("Intersex", "intersex"),
+					huh.NewOption("?", "?"),
+					huh.NewOption("Other", "other"),
+				).
+				Value(&sex),
+
+			huh.NewInput().
+				Title("List known languages:").
+				Value(&languages),
+			),
+
+		huh.NewGroup(
+			huh.NewText().
+				Title("Describe your NPC").
+				Value(&desc),
+		),
+
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Level").
+				Value(&level).
+				Validate(func(s string) error {
+					_, err := strconv.ParseInt(s, 10, 64)
+					if err != nil {
+						return fmt.Errorf("%s is not a number!", s)
+					}
+					return nil
+				}),
+
+			huh.NewInput().
+				Title("Hitpoints").
+				Value(&hitpoints).
+				Validate(func(s string) error {
+					_, err := strconv.ParseInt(s, 10, 64)
+					if err != nil {
+						return fmt.Errorf("%s is not a number!", s)
+					}
+					return nil
+				}),
+		),
+	)
+
+	err := npcForm.Run()
+	if err != nil {
+		return err
+	}
+
+	parsedLevel, _ := strconv.ParseInt(level, 10, 64)
+	parsedHP, _ := strconv.ParseInt(hitpoints, 10, 64)
+		
+	npc := &db.NpcParams{
+		Name: name,
+		Race: race,
+		Class: class,
+		Subclass: subclass,
+		Alignment: alignment,
+		Sex: sex,
+		Description: desc,
+		Languages: languages,
+		Level: int(parsedLevel),
+		Hitpoints: int(parsedHP),
+	}
+
+	added, err := s.Db.AddNpc(context.Background(), npc)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println(added)
+
+	return nil
+			
 }
--- go.mod
+++ go.mod
@@ -9,16 +9,28 @@ require (
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.20.0 // indirect
+	github.com/charmbracelet/bubbletea v1.1.0 // indirect
+	github.com/charmbracelet/huh v0.6.0 // indirect
 	github.com/charmbracelet/x/ansi v0.4.2 // indirect
+	github.com/charmbracelet/x/exp/strings v0.0.0-20240722160745-212f7b056ed0 // indirect
+	github.com/charmbracelet/x/term v0.2.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
-	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
@@ -26,6 +38,7 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
 	modernc.org/libc v1.55.3 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
