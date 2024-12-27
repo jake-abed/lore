38e5d616f51e4a213087672911aae0c6bf6b1d68
Jake Abed <jakeabed@proton.me>
2024-12-27 11:35:35 -0800

    Increase main test coverage

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestMain(t *testing.T) {
+func TestMainDefault(t *testing.T) {
 	origOut := os.Stdout
 	r, w, _ := os.Pipe()
 	os.Args = []string{"auxquest"}
@@ -23,6 +23,28 @@ func TestMain(t *testing.T) {
 		"*** -i",
 		"- View all monsters on the D&D 5e OpenAPI.",
 		"Get information about all available commands.",
+	}
+	for _, phrase := range expected {
+		if !strings.Contains(readableOutput, phrase) {
+			t.Errorf("Expected StdOut to contain %s", phrase)
+		}
+	}
+}
+
+func TestMainMonsters(t *testing.T) {
+	origOut := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Args = []string{"auxquest", "monsters"}
+	os.Stdout = w
+	main()
+	os.Stdout = origOut
+	w.Close()
+	out, _ := io.ReadAll(r)
+	readableOutput := string(out)
+	expected := []string{
+		"AuxQuest Monsters Help",
+		"Monsters subcommands information",
+		"*** monsters -i <monster-name>",
 	}
 	for _, phrase := range expected {
 		if !strings.Contains(readableOutput, phrase) {
