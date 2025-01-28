007e6766c808a65dbf0fefae6e6267a1b9883ae3
Jake Abed <jakeabed@proton.me>
2025-01-27 22:18:57 -0800

    Finish v1 of dice subcommand

--- commands/command_dice.go
+++ commands/command_dice.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"math/rand/v2"
 
 	"github.com/jake-abed/lore/internal/dice"
 )
@@ -9,19 +10,46 @@ import (
 func commandDice(s *State) error {
 	diceArgs := s.Args[1:]
 
+	if len(diceArgs) == 0 {
+		return fmt.Errorf("The dice command requires at least one argument!")
+	}
+
 	var flag string
 	var diceExpression string
 
 	if len(diceArgs) == 1 && diceArgs[0][0] != 45 {
-		fmt.Println("just default roll")
 		flag = "-a"
 		diceExpression = diceArgs[0]
+	} else {
+		flag = diceArgs[0]
+		diceExpression = diceArgs[1]
 	}
 
-	fmt.Println(flag, diceExpression)
-
-	fmt.Println(diceArgs)
-	fmt.Println(dice.SumRollDice("2d6-1"))
-	fmt.Println("Dice command place holder!")
+	switch flag {
+	case "-a":
+		num, dieVal, modifier := dice.ParseDiceExpression(diceExpression)
+		var sum int
+		for range num {
+			sum += rand.IntN(int(dieVal)) + 1
+		}
+		msg := fmt.Sprintf("Result of %s roll: ", diceExpression)
+		msg = bold.Render(msg)
+		fmt.Println(msg, sum+int(modifier))
+	case "-i":
+		num, dieVal, modifier := dice.ParseDiceExpression(diceExpression)
+		var sum int
+		for i := range num {
+			roll := rand.IntN(int(dieVal)) + 1
+			sum += roll
+			msg := fmt.Sprintf("Roll %d: ", i+1)
+			msg = bold.Render(msg)
+			fmt.Println(msg, roll)
+		}
+		msg := fmt.Sprintf("Sum of all rolls & modifier: ")
+		msg = bold.Render(msg)
+		fmt.Println(msg, sum+int(modifier))
+	default:
+		return fmt.Errorf("Unknown flag for dice subcommand!")
+	}
 	return nil
 }
