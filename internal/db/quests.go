abfa615300bcba50226e6a061be525381851f214
Jake Abed <jakeabed@proton.me>
2025-01-29 21:25:17 -0800

    Add quest db types and addquest

---
+++ internal/db/quests.go
@@ -0,0 +1,114 @@
+package db
+
+import (
+	"context"
+)
+
+type Quest struct {
+	Name        string
+	Desc        string
+	Rewards     string
+	Notes       string
+	Level       int
+	IsStarted   bool
+	IsFinished  bool
+	CurrentStep int
+	WorldId     int
+	Id          int
+}
+
+type QuestParams struct {
+	Name        string
+	Desc        string
+	Rewards     string
+	Notes       string
+	Level       int
+	IsStarted   int
+	IsFinished  int
+	CurrentStep int
+	WorldId     int
+}
+
+type QuestStep struct {
+	Name       string
+	Type       string
+	Desc       string
+	Reward     string
+	IsStarted  bool
+	IsFinished bool
+	QuestId    int
+	WorldId    int
+}
+
+type QuestStepParams struct {
+	Name       string
+	Type       string
+	Desc       string
+	Reward     string
+	IsStarted  bool
+	IsFinished bool
+	QuestId    int
+	WorldId    int
+}
+
+const createQuestQuery = `
+INSERT INTO quests (
+	name,
+	description,
+	rewards,
+	notes,
+	level,
+	is_started,
+  is_finished,
+  current_step,
+  world_id
+) VALUES (
+	$1,
+	$2,
+	$3,
+	$4,
+	$5,
+	$6,
+	$7,
+	$8,
+	$9
+) RETURNING *
+`
+
+func (q *Queries) AddQuest(
+	ctx context.Context,
+	p *QuestParams,
+) (*Quest, error) {
+	quest := Quest{}
+	row := q.Db.QueryRowContext(
+		ctx,
+		createQuestQuery,
+		p.Name,
+		p.Desc,
+		p.Rewards,
+		p.Notes,
+		p.Level,
+		p.IsStarted,
+		p.IsFinished,
+		p.CurrentStep,
+		p.WorldId,
+	)
+
+	err := row.Scan(
+		&quest.Id,
+		&quest.Name,
+		&quest.Desc,
+		&quest.Rewards,
+		&quest.Notes,
+		&quest.Level,
+		&quest.IsStarted,
+		&quest.IsFinished,
+		&quest.CurrentStep,
+		&quest.WorldId,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &quest, nil
+}
